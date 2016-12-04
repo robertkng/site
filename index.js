917693e531643723fe171081aa8352875bdf3306
Robert Ng <robertng85@gmail.com>
2016-12-04 18:06:22 -0500

    scroll up button added

--- index.js
+++ index.js
@@ -6,3 +6,28 @@
 //   });
 // });
 
+
+$(document).ready(function() {
+  const btt = $('.top');
+
+  btt.on('click', function(e) {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 300);
+    e.preventDefault();
+  });
+
+  $(window).on('scroll', function() {
+    const self = $(this),
+          height = self.height(),
+          top = self.scrollTop();
+
+          if (top > height) {
+            if(!btt.is(':visible')) {
+              btt.show();
+            }
+          } else {
+            btt.hide();
+          }
+  });
+});
